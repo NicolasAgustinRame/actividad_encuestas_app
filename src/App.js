fac925528b09792fa261ddd38fe1122dddd8ef94
114186_RameNicolas <109234443+NicolasAgustinRame@users.noreply.github.com>
2023-09-17 16:24:18 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Encuestas from './components/Encuesta';
 
 function App() {
+  const encuestas = [
+    {id: 1, pregunta: "¿Cual es tu color favorito?", opciones: ['Rojo', 'Verde', 'Azul']},
+    {id: 2, pregunta: "¿Cual es tu comida favorita?", opciones: ['Pizza', 'Hamburguesa', 'Sushi']},
+  ];
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Aplicacion de Encuestas</h1>
+      <Encuestas encuestas={encuestas}/>
     </div>
   );
 }
